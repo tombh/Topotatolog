c14164cdd5ee7aa7a4b45400cd5c62c04a9142fc
tombh <tom@tombh.co.uk>
2011-12-05 17:41:16 +0000

    styling done.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from blog.models import Post
 from django.shortcuts import get_object_or_404
 from django.views.generic.simple import direct_to_template
+from django.contrib import messages
 
 def index(request):    
     return direct_to_template(
@@ -9,7 +10,9 @@ def index(request):
         'posts': Post.objects.all()[:5]
     })
 
-def view_post(request, slug):    
+def view_post(request, slug):
+    if request.GET.get('c'):
+        messages.info(request, "Thanks for your comment")
     return direct_to_template(
         request,
         'view_post.html',
